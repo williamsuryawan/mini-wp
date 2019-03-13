db22f44f594ddb289b65c257da598e1c4e158788
William Suryawan <suryawan.william@gmail.com>
2019-03-13 14:36:12 +0700

    update env for port in app.js

--- server/app.js
+++ server/app.js
@@ -6,11 +6,11 @@ const mongoose = require ('mongoose')
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
 const articleRouter = require('./routes/articles')
-const port = 3000
+const port = process.env.port || 3000
 
 const app = express()
 app.use(cors())
-mongoose.connect('mongodb+srv://williamsuryawan:1234@cluster0-dlbfv.mongodb.net/test?retryWrites=true', {useNewUrlParser: true})
+mongoose.connect(`mongodb+srv://${process.env.name}:${process.env.password}@cluster0-dlbfv.mongodb.net/test?retryWrites=true`, {useNewUrlParser: true})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
